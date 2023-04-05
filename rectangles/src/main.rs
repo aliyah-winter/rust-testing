475d371253554e9fe5ad421ead3697dfb2232b08
Aliyah Winter <aliyahgwinter@gmail.com>
2023-04-05 13:59:34 +1200

    implement can_hold

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,10 +8,11 @@ impl Rectangle {
   fn area(&self) -> u32 {
     self.width * self.height
   }
-}
-impl Rectangle {
   fn width(&self) -> bool {
     self.width > 0
+  }
+  fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
   }
 }
 
@@ -20,14 +21,28 @@ fn main() {
     width: 30,
     height: 50,
   };
+  let rect2 = Rectangle {
+    width: 10,
+    height: 40,
+  };
+  let rect3 = Rectangle {
+    width: 60,
+    height: 45,
+  };
 
   println!("rect1 is {:#?}", rect1);
+
   println!(
       "The area of the rectangle is {} square pixels.", rect1.area()
   );
+
   if rect1.width() {
     println!(
       "Width is nonzero, it is {}", rect1.width()
   )
 };
+
+println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+
+println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
