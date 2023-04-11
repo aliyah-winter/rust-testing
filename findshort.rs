d1db64e47f2f2b914e4a7b536139a63475781b22
Aliyah Winter <aliyahgwinter@gmail.com>
2023-04-11 15:08:19 +1200

    refactor

--- findshort.rs
+++ findshort.rs
@@ -5,12 +5,11 @@ fn main() {
 
 fn find_short(s: &str) -> u32 {
   let v: Vec<&str> = s.split(' ').collect();
-  let shortest = v.iter().fold(v[0], |acc: &str, &item| {
+  v.iter().fold(v[0], |acc: &str, &item| {
     if item.len() < acc.len() {
       item
     } else {
       acc
     }
-  });
-  shortest.len() as u32
+  }).len() as u32
 }
