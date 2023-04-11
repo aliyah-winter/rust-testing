b47a33fe8d8484f7c2560e2189af6d9268aa8f60
Aliyah Winter <aliyahgwinter@gmail.com>
2023-04-11 15:12:38 +1200

    refactor as map/min solution

--- findshort.rs
+++ findshort.rs
@@ -4,12 +4,16 @@ fn main() {
 }
 
 fn find_short(s: &str) -> u32 {
-  let v: Vec<&str> = s.split(' ').collect();
-  v.iter().fold(v[0], |acc: &str, &item| {
-    if item.len() < acc.len() {
-      item
-    } else {
-      acc
-    }
-  }).len() as u32
+  // let v: Vec<&str> = s.split(' ').collect();
+  // v.iter().fold(v[0], |acc: &str, &item| {
+  //   if item.len() < acc.len() {
+  //     item
+  //   } else {
+  //     acc
+  //   }
+  // }).len() as u32
+  match s.split_whitespace().map(|word| word.len()).min() {
+    Some(min) => min as u32,
+    None => 0u32
+  }
 }
