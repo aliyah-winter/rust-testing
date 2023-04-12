c78f10851ff8b2f69b17fb927c6c45df4c924d16
Aliyah Winter <aliyahgwinter@gmail.com>
2023-04-12 15:21:56 +1200

    refactor to one line

--- odd-or-even.rs
+++ odd-or-even.rs
@@ -2,9 +2,8 @@ fn main() {
   println!("{}", odd_or_even([0, -1, -5].to_vec()))
 }
 fn odd_or_even(numbers: Vec<i32>) -> String {
-  let res: i32 = numbers.iter().fold(0, |acc, num| acc + num);
-  match res {
-    _ if res % 2 == 0 => String::from("even"),
-    _ => String::from("odd")
+  match numbers.iter().sum::<i32>() % 2 == 0 {
+    true => String::from("even"),
+    false => String::from("odd")
   }
 }
