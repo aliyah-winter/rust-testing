8fe3a55db3352d814075e01b29743ba8b2932f76
Aliyah Winter <aliyahgwinter@gmail.com>
2023-04-17 15:02:43 +1200

    complete enums ip address

--- enums.rs
+++ enums.rs
@@ -1,11 +1,9 @@
 fn main() {
   enum IpAddrKind {
-    V4,
-    V6,
+    V4(u8, u8, u8, u8),
+    V6(String),
   }
 
-  let four = IpAddrKind::V4;
-  let six = IpAddrKind::V6;
+  let home = IpAddrKind::V4(127, 0, 0, 1);
+  let loopback = IpAddrKind::V6(String::from("::1"));
 }
-
-fn route(ip_kind: IpAddrKind) {}
