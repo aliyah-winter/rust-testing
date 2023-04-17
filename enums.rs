d0a9c094c3c21c60ef346dd2a29c929987cc8305
Aliyah Winter <aliyahgwinter@gmail.com>
2023-04-17 15:18:01 +1200

    finish enums testing

--- enums.rs
+++ enums.rs
@@ -6,4 +6,17 @@ fn main() {
 
   let home = IpAddrKind::V4(127, 0, 0, 1);
   let loopback = IpAddrKind::V6(String::from("::1"));
+
+enum Message {
+  Quit,
+  Move {x: i32, y: i32},
+  Write(String),
+  ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+  fn call(&self) {
+    
+}
+
 }
