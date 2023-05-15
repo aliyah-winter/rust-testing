d34f5ecd59725724b442e1aa8b7b73eefae1e480
Aliyah Winter <aliyahgwinter@gmail.com>
2023-05-15 16:14:03 +1200

    refactor for mutable parameter

--- growth.rs
+++ growth.rs
@@ -2,13 +2,12 @@ fn main() {
   println!("{}", nb_year(1500, 5.0, 100, 5000))
 }
 
-fn nb_year(p0: i32, percent: f64, aug: i32, p: i32)-> i32 {
-  let mut total: i32 = p0;
-  let mut years: i32 = 0;
-  
- while total < p { 
-  total = total + (total as f64 * percent / 100.0) as i32 + aug;
-  years += 1;
+fn nb_year(mut p0: i32, percent: f64, aug: i32, p: i32)-> i32 {
+  let mut year_count: i32 = 0;
+ while p0 < p { 
+  p0 = p0 + (p0 as f64 * percent / 100.0) as i32 + aug;
+  year_count += 1;
 }
-  years
+  year_count
+
 }
